c7d990fa941f1b2e170c766978ef1fd02382ae70
Kamurai <Kamurasijordanarthur@gmail.com>
2026-03-06 19:19:20 +0300

    Porting of freeRtos success

---
+++ web_sever_port/FSBL/Appli/App/main_app.c
@@ -0,0 +1,367 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    main_app.c
+  * @author  GPM Application Team
+  * @brief   main_app program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+/* Application */
+#include "main.h"
+#include "main_app.h"
+#include "app_config.h"
+
+#if (LOW_POWER_MODE > LOW_POWER_DISABLE)
+#include "utilities_conf.h"
+#include "stm32_lpm.h"
+#endif /* LOW_POWER_MODE */
+
+#include "w6x_api.h"
+#include "spi_iface.h" /* SPI falling/rising_callback */
+#include "logging.h"
+#include "shell.h"
+#include "logshell_ctrl.h"
+#if (LOW_POWER_MODE == LOW_POWER_STDBY_ENABLE)
+#error "low power standby mode not supported"
+#endif /* LOW_POWER_MODE */
+
+/* USER CODE BEGIN Includes */
+#include "qpc.h"
+#include "HttpServer.h"
+#include "common_parser.h"
+/* USER CODE END Includes */
+
+/* Global variables ----------------------------------------------------------*/
+/* USER CODE BEGIN GV */
+
+/* USER CODE END GV */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private defines -----------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macros ------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/**
+  * @brief  Wi-Fi event callback
+  * @param  event_id: Event ID
+  * @param  event_args: Event arguments
+  */
+static void APP_wifi_cb(W6X_event_id_t event_id, void *event_args);
+
+/**
+  * @brief  Network event callback
+  * @param  event_id: Event ID
+  * @param  event_args: Event arguments
+  */
+static void APP_net_cb(W6X_event_id_t event_id, void *event_args);
+
+/**
+  * @brief  MQTT event callback
+  * @param  event_id: Event ID
+  * @param  event_args: Event arguments
+  */
+static void APP_mqtt_cb(W6X_event_id_t event_id, void *event_args);
+
+/**
+  * @brief  BLE event callback
+  * @param  event_id: Event ID
+  * @param  event_args: Event arguments
+  */
+static void APP_ble_cb(W6X_event_id_t event_id, void *event_args);
+
+/**
+  * @brief  W6X error callback
+  * @param  ret_w6x: W6X status
+  * @param  func_name: function name
+  */
+static void APP_error_cb(W6X_Status_t ret_w6x, char const *func_name);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Functions Definition ------------------------------------------------------*/
+void main_app(void)
+{
+  int32_t ret = 0;
+
+  /* USER CODE BEGIN main_app_1 */
+
+  /* USER CODE END main_app_1 */
+
+  /* Initialize the logging utilities */
+  LoggingInit();
+
+  /* Initialize the shell utilities on UART instance */
+  ShellInit();
+
+  /* USER CODE BEGIN main_app_2 */
+
+  /* USER CODE END main_app_2 */
+
+  /* Register the application callback to received events from ST67W6X Driver */
+  W6X_App_Cb_t App_cb = {0};
+  App_cb.APP_wifi_cb = APP_wifi_cb;
+  App_cb.APP_net_cb = APP_net_cb;
+  App_cb.APP_ble_cb = APP_ble_cb;
+  App_cb.APP_mqtt_cb = APP_mqtt_cb;
+  App_cb.APP_error_cb = APP_error_cb;
+  W6X_RegisterAppCb(&App_cb);
+
+  /* Initialize the ST67W6X Driver */
+  ret = W6X_Init();
+  if (ret)
+  {
+    LogError("Failed to initialize ST67W6X Driver, %" PRIi32 "\n", ret);
+    goto _err;
+  }
+
+  /* Initialize the ST67W6X Wi-Fi module */
+  ret = W6X_WiFi_Init();
+  if (ret)
+  {
+    LogError("Failed to initialize ST67W6X Wi-Fi component, %" PRIi32 "\n", ret);
+    goto _err;
+  }
+  LogInfo("Wi-Fi init is done\n");
+
+  /* Initialize the ST67W6X Network module */
+  ret = W6X_Net_Init();
+  if (ret)
+  {
+    LogError("Failed to initialize ST67W6X Net component, %" PRIi32 "\n", ret);
+    goto _err;
+  }
+  LogInfo("Net init is done\n");
+
+  /* USER CODE BEGIN main_app_3 */
+
+  //Create the Alert the server AO about the failure
+  QACTIVE_POST(AO_Server, Q_NEW(QEvt, SERVER_LISTEN_SIG), (void)0U);
+
+  return;
+  /* USER CODE END main_app_3 */
+
+  LogInfo("##### Quitting the application\n");
+
+  /* USER CODE BEGIN main_app_Last */
+
+  /* USER CODE END main_app_Last */
+
+_err:
+  /* USER CODE BEGIN main_app_Err_1 */
+
+  /* USER CODE END main_app_Err_1 */
+  /* De-initialize the ST67W6X Network module */
+  W6X_Net_DeInit();
+
+  /* De-initialize the ST67W6X Wi-Fi module */
+  W6X_WiFi_DeInit();
+
+  /* De-initialize the ST67W6X Driver */
+  W6X_DeInit();
+
+  shell_freertos_deinit();
+
+  /* USER CODE BEGIN main_app_Err_2 */
+
+  //Create the Alert the server AO abou the failure
+  QACTIVE_POST(AO_Server, Q_NEW(QEvt, ST67_INIT_FAIL_SIG), (void)0U);
+
+
+  return;
+
+  /* USER CODE END main_app_Err_2 */
+  LogInfo("##### Application end\n");
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t pin);
+void HAL_GPIO_EXTI_Rising_Callback(uint16_t pin);
+void HAL_GPIO_EXTI_Falling_Callback(uint16_t pin);
+
+void HAL_GPIO_EXTI_Callback(uint16_t pin)
+{
+  /* USER CODE BEGIN HAL_GPIO_EXTI_Callback_1 */
+#if 1
+  /* USER CODE END HAL_GPIO_EXTI_Callback_1 */
+  /* Callback when data is available in Network CoProcessor to enable SPI Clock */
+  if (pin == SPI_RDY_Pin)
+  {
+    if (HAL_GPIO_ReadPin(SPI_RDY_GPIO_Port, SPI_RDY_Pin) == GPIO_PIN_SET)
+    {
+      HAL_GPIO_EXTI_Rising_Callback(pin);
+    }
+    else
+    {
+      HAL_GPIO_EXTI_Falling_Callback(pin);
+    }
+  }
+  /* USER CODE BEGIN HAL_GPIO_EXTI_Callback_End */
+#else
+#endif /* 1 */
+  /* USER CODE END HAL_GPIO_EXTI_Callback_End */
+}
+
+void HAL_GPIO_EXTI_Rising_Callback(uint16_t pin)
+{
+  /* USER CODE BEGIN EXTI_Rising_Callback_1 */
+#if 1
+  /* USER CODE END EXTI_Rising_Callback_1 */
+  /* Callback when data is available in Network CoProcessor to enable SPI Clock */
+  if (pin == SPI_RDY_Pin)
+  {
+    spi_on_txn_data_ready();
+  }
+  /* USER CODE BEGIN EXTI_Rising_Callback_End */
+#else
+#endif /* 1 */
+  /* USER CODE END EXTI_Rising_Callback_End */
+}
+
+void HAL_GPIO_EXTI_Falling_Callback(uint16_t pin)
+{
+  /* USER CODE BEGIN EXTI_Falling_Callback_1 */
+#if 1
+  /* USER CODE END EXTI_Falling_Callback_1 */
+  /* Callback when data is available in Network CoProcessor to enable SPI Clock */
+  if (pin == SPI_RDY_Pin)
+  {
+    spi_on_header_ack();
+  }
+  /* USER CODE BEGIN EXTI_Falling_Callback_End */
+#else
+#endif /* 1 */
+  /* USER CODE END EXTI_Falling_Callback_End */
+}
+
+/* USER CODE BEGIN FD */
+
+/* USER CODE END FD */
+
+/* Private Functions Definition ----------------------------------------------*/
+static void APP_wifi_cb(W6X_event_id_t event_id, void *event_args)
+{
+  /* USER CODE BEGIN APP_wifi_cb_1 */
+
+	  W6X_WiFi_CbParamData_t *cb_data = {0};
+
+	  switch (event_id)
+	  {
+	    case W6X_WIFI_EVT_CONNECTED_ID:
+	      break;
+
+	    case W6X_WIFI_EVT_DISCONNECTED_ID:
+	      break;
+
+	    case W6X_WIFI_EVT_REASON_ID:
+	      LogInfo("Reason: %s\n", W6X_WiFi_ReasonToStr(event_args));
+	      break;
+
+	    case W6X_WIFI_EVT_DIST_STA_IP_ID:
+	      cb_data = (W6X_WiFi_CbParamData_t *)event_args;
+	      LogInfo(" Station connected to soft-AP + IP : [" MACSTR "] IP: " IPSTR "\n",
+	              MAC2STR(cb_data->MAC), IP2STR(cb_data->IP));
+	      break;
+
+	    case W6X_WIFI_EVT_STA_CONNECTED_ID:
+	#if 0
+	      cb_data = (W6X_WiFi_CbParamData_t *)event_args;
+	      LogInfo("Station connected to soft-AP : [" MACSTR "]\n", MAC2STR(cb_data->MAC));
+	#endif /* 0 */
+	      break;
+
+	    case W6X_WIFI_EVT_STA_DISCONNECTED_ID:
+	      cb_data = (W6X_WiFi_CbParamData_t *)event_args;
+	      LogInfo(" Station disconnected from soft-AP : [" MACSTR "]\n", MAC2STR(cb_data->MAC));
+	      break;
+
+	    default:
+	      break;
+	  }
+
+  /* USER CODE END APP_wifi_cb_1 */
+}
+
+static void APP_net_cb(W6X_event_id_t event_id, void *event_args)
+{
+  /* USER CODE BEGIN APP_net_cb_1 */
+
+  W6X_Net_CbParamData_t *p_param_app_net_cb;
+
+  switch (event_id)
+  {
+	case W6X_NET_EVT_SOCK_DATA_ID:
+	  p_param_app_net_cb = (W6X_Net_CbParamData_t *) event_args;
+	  LogInfo(" Cb informed app that Wi-Fi %" PRIu32 " bytes available on socket %" PRIu32 ".\n",
+			  p_param_app_net_cb->available_data_length, p_param_app_net_cb->socket_id);
+	  break;
+
+	default:
+	  break;
+  }
+
+  /* USER CODE END APP_net_cb_1 */
+}
+
+static void APP_mqtt_cb(W6X_event_id_t event_id, void *event_args)
+{
+  /* USER CODE BEGIN APP_mqtt_cb_1 */
+
+  /* USER CODE END APP_mqtt_cb_1 */
+}
+
+static void APP_ble_cb(W6X_event_id_t event_id, void *event_args)
+{
+  /* USER CODE BEGIN APP_ble_cb_1 */
+
+  /* USER CODE END APP_ble_cb_1 */
+}
+
+static void APP_error_cb(W6X_Status_t ret_w6x, char const *func_name)
+{
+  /* USER CODE BEGIN APP_error_cb_1 */
+
+  /* USER CODE END APP_error_cb_1 */
+  LogError("[%s] in %s API\n", W6X_StatusToStr(ret_w6x), func_name);
+  /* USER CODE BEGIN APP_error_cb_2 */
+
+  /* USER CODE END APP_error_cb_2 */
+}
+
+/* USER CODE BEGIN PFD */
+
+/* USER CODE END PFD */
---
+++ web_sever_port/FSBL/Core/Src/main.c
@@ -0,0 +1,417 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os2.h"
+#include "csi.h"
+#include "gpdma.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
+#include "app_st67w6x.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "qpc.h"
+#include "httpServer.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+extern DMA_HandleTypeDef handle_GPDMA1_Channel10 ;
+extern DMA_HandleTypeDef handle_GPDMA1_Channel11 ;
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void MX_FREERTOS_Init(void);
+static void SystemIsolation_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* Enable the CPU Cache */
+
+  /* Enable I-Cache---------------------------------------------------------*/
+  SCB_EnableICache();
+
+  /* Enable D-Cache---------------------------------------------------------*/
+  SCB_EnableDCache();
+
+  /* MCU Configuration--------------------------------------------------------*/
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+//  Disable  all intterrupts
+  __disable_irq();
+
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_GPDMA1_Init();
+  MX_SPI5_Init();
+  MX_USART1_UART_Init();
+  SystemIsolation_Config();
+  /* USER CODE BEGIN 2 */
+  // initialize event pools
+  static QF_MPOOL_EL(QEvt) smlPoolSto[10U]; // small pool
+  QF_poolInit(smlPoolSto, sizeof(smlPoolSto), sizeof(smlPoolSto[0]));
+
+
+  static QEvt const *serverQueueSto[SERVER_QUEUE_EN];
+  static StackType_t serverStack[SERVER_TASK_STACK_SIZE];
+
+  Server_ctor(); // instantiate the Server active object
+
+  QActive_setAttr(AO_Server, TASK_NAME_ATTR, "Server");
+  QACTIVE_START(AO_Server, // AO to start
+  SERVER_TASK_PRIORITY, // QP priority of the AO
+  serverQueueSto, // event queue storage
+  Q_DIM(serverQueueSto), // queue length [events]
+  serverStack, // stack storage
+  sizeof(serverStack), // stack size [bytes]
+  (QEvt *)0); // initialization param (not used)
+
+  return QF_run(); // run the QF application
+
+  Error_Handler();
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+  /* Call init function for freertos objects (in app_freertos.c) */
+  MX_FREERTOS_Init();
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+//	  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+//	  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+/* USER CODE BEGIN CLK 1 */
+/* USER CODE END CLK 1 */
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the System Power Supply
+  */
+  if (HAL_PWREx_ConfigSupply(PWR_EXTERNAL_SOURCE_SUPPLY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Enable HSI */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL1.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL2.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL3.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL4.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes TIMPRE when TIM is used as Systick Clock Source
+  */
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_TIM;
+  PeriphClkInitStruct.TIMPresSelection = RCC_TIMPRES_DIV2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Get current CPU/System buses clocks configuration and if necessary switch
+ to intermediate HSI clock to ensure target clock can be set
+  */
+  HAL_RCC_GetClockConfig(&RCC_ClkInitStruct);
+  if ((RCC_ClkInitStruct.CPUCLKSource == RCC_CPUCLKSOURCE_IC1) ||
+     (RCC_ClkInitStruct.SYSCLKSource == RCC_SYSCLKSOURCE_IC2_IC6_IC11))
+  {
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_CPUCLK | RCC_CLOCKTYPE_SYSCLK);
+    RCC_ClkInitStruct.CPUCLKSource = RCC_CPUCLKSOURCE_HSI;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
+    {
+      /* Initialization Error */
+      Error_Handler();
+    }
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+  RCC_OscInitStruct.PLL1.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL1.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL1.PLLM = 1;
+  RCC_OscInitStruct.PLL1.PLLN = 25;
+  RCC_OscInitStruct.PLL1.PLLFractional = 0;
+  RCC_OscInitStruct.PLL1.PLLP1 = 1;
+  RCC_OscInitStruct.PLL1.PLLP2 = 1;
+  RCC_OscInitStruct.PLL2.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL3.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL4.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL4.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL4.PLLM = 1;
+  RCC_OscInitStruct.PLL4.PLLN = 25;
+  RCC_OscInitStruct.PLL4.PLLFractional = 0;
+  RCC_OscInitStruct.PLL4.PLLP1 = 1;
+  RCC_OscInitStruct.PLL4.PLLP2 = 1;
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_CPUCLK|RCC_CLOCKTYPE_HCLK
+                              |RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1
+                              |RCC_CLOCKTYPE_PCLK2|RCC_CLOCKTYPE_PCLK5
+                              |RCC_CLOCKTYPE_PCLK4;
+  RCC_ClkInitStruct.CPUCLKSource = RCC_CPUCLKSOURCE_IC1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_IC2_IC6_IC11;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+  RCC_ClkInitStruct.APB5CLKDivider = RCC_APB5_DIV1;
+  RCC_ClkInitStruct.IC1Selection.ClockSelection = RCC_ICCLKSOURCE_PLL1;
+  RCC_ClkInitStruct.IC1Selection.ClockDivider = 2;
+  RCC_ClkInitStruct.IC2Selection.ClockSelection = RCC_ICCLKSOURCE_PLL1;
+  RCC_ClkInitStruct.IC2Selection.ClockDivider = 4;
+  RCC_ClkInitStruct.IC6Selection.ClockSelection = RCC_ICCLKSOURCE_PLL1;
+  RCC_ClkInitStruct.IC6Selection.ClockDivider = 4;
+  RCC_ClkInitStruct.IC11Selection.ClockSelection = RCC_ICCLKSOURCE_PLL1;
+  RCC_ClkInitStruct.IC11Selection.ClockDivider = 8;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief RIF Initialization Function
+  * @param None
+  * @retval None
+  */
+  static void SystemIsolation_Config(void)
+{
+
+/* USER CODE BEGIN RIF_Init 0 */
+
+/* USER CODE END RIF_Init 0 */
+
+  /* set all required IPs as secure privileged */
+  __HAL_RCC_RIFSC_CLK_ENABLE();
+
+  /* RIF-Aware IPs Config */
+
+  /* set up GPDMA configuration */
+  /* set GPDMA1 channel 10 used by SPI5 */
+  if (HAL_DMA_ConfigChannelAttributes(&handle_GPDMA1_Channel10,DMA_CHANNEL_SEC|DMA_CHANNEL_PRIV|DMA_CHANNEL_SRC_SEC|DMA_CHANNEL_DEST_SEC)!= HAL_OK )
+  {
+    Error_Handler();
+  }
+  /* set GPDMA1 channel 11 used by SPI5 */
+  if (HAL_DMA_ConfigChannelAttributes(&handle_GPDMA1_Channel11,DMA_CHANNEL_SEC|DMA_CHANNEL_PRIV|DMA_CHANNEL_SRC_SEC|DMA_CHANNEL_DEST_SEC)!= HAL_OK )
+  {
+    Error_Handler();
+  }
+
+/* USER CODE BEGIN RIF_Init 1 */
+
+/* USER CODE END RIF_Init 1 */
+/* USER CODE BEGIN RIF_Init 2 */
+
+/* USER CODE END RIF_Init 2 */
+
+}
+
+/* USER CODE BEGIN 4 */
+  // QF callbacks...
+  void QF_onStartup(void) {
+      // set up the SysTick timer to fire at BSP_TICKS_PER_SEC rate
+      //SysTick_Config(SystemCoreClock / BSP_TICKS_PER_SEC); // done in FreeRTOS
+
+      // assign all priority bits for preemption-prio. and none to sub-prio.
+//      NVIC_SetPriorityGrouping(0U);
+
+      // set priorities of ALL ISRs used in the system, see NOTE1
+//      NVIC_SetPriority(USART2_IRQn,    0U); // kernel UNAWARE interrupt
+//      NVIC_SetPriority(SysTick_IRQn,   RTOS_AWARE_ISR_CMSIS_PRI + 0U);
+      // ...
+
+      // enable IRQs...
+	  __enable_irq();
+
+  #ifdef Q_SPY
+      NVIC_EnableIRQ(USART2_IRQn); // USART2 interrupt used for QS-RX
+  #endif
+  }
+  //............................................................................
+  void QF_onCleanup(void) {
+
+  }
+
+  Q_NORETURN Q_onError(char const * const module, int_t const id) {
+      // NOTE: this implementation of the error handler is intended only
+      // for debugging and MUST be changed for deployment of the application
+      // (assuming that you ship your production code with assertions enabled).
+      Q_UNUSED_PAR(module);
+      Q_UNUSED_PAR(id);
+      QS_ASSERTION(module, id, 10000U); // report assertion to QS
+
+  #ifndef NDEBUG
+      // for debugging, hang on in an endless loop...
+      Error_Handler();
+  #endif
+
+      NVIC_SystemReset();
+      for (;;) { // explicitly "no-return"
+      }
+  }
+/* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+//  __disable_irq();
+  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
